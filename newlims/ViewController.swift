996d346d41f02dcaa1e6f9e53894ef033a0b5e9e
sajad khajeh <sajad@sajads-mac.local>
2018-10-17 22:09:54 -0700

    Initial Commit

---
+++ newlims/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  newlims
+//
+//  Created by sajad khajeh on 10/17/18.
+//  Copyright © 2018 NSCOM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
